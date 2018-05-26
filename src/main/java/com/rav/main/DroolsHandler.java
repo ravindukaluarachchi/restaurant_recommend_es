d5f2cff493eb62b537220534f20d91e455df367c
Ravindu Kaluarachchi <raoffire@yahoo.com>
2018-05-26 21:14:17 +0530

    Clean code.

--- src/main/java/com/rav/main/DroolsHandler.java
+++ src/main/java/com/rav/main/DroolsHandler.java
@@ -21,7 +21,6 @@ public class DroolsHandler {
 	
 	public List<Restaurant> handle(Query query) throws IOException {
 		List<Restaurant> restaurants = new ArrayList<>();
-		// List<Fact> facts = new ArrayList<>();
 		List<String> cities = new ArrayList<>();
 
 		ClassLoader classLoader = getClass().getClassLoader();
@@ -48,53 +47,35 @@ public class DroolsHandler {
 
 			restaurants.get(restaurants.indexOf(new Restaurant(parts[0], parts[1]))).getItems()
 					.add(new Item(parts[2], 0f));
-			// System.out.println(restaurants.get(restaurants.indexOf(new
-			// Restaurant(parts[0], parts[1]))).getItems().contains("pizza"));
+		
 		});
 		List<Item> items = new ArrayList<>();
 		items.add(new Item("pizza", 0f));
 		System.out.println(items.contains("pizza"));
 
-		/*
-		 * cities.forEach(c -> { facts.add(new Fact(Restaurant.DELIVERY + "_" + c, c));
-		 * facts.add(new Fact(Restaurant.DINE_IN + "_" + c, c)); });
-		 * 
-		 * restaurants.forEach(r -> { facts.add(new Fact(r.getName(), r.getDineType()));
-		 * r.getItems().forEach(i -> { facts.add(new Fact(i.getName(), r.getName()));
-		 * });
-		 * 
-		 * });
-		 */
 
 		Result result = new Result();
-		// load up the knowledge base
+		
 		KieServices ks = KieServices.Factory.get();
 		KieContainer kContainer = ks.getKieClasspathContainer();
 		final KieSession kSession = kContainer.newKieSession("ksession-rules");
 
 		kSession.setGlobal("result", result);
 		kSession.setGlobal("q", query);
-
-		/*
-		 * facts.forEach(f -> { System.out.println(f); kSession.insert(f); });
-		 */
+	
 
 		restaurants.forEach(r -> {
 			kSession.insert(r);
 		});
-		System.out.println("----------------------------------------");
+		
 
 		kSession.fireAllRules();
 
-		restaurants.sort((r1, r2) -> r2.getRate() - r1.getRate());
-		// System.out.println(result.getValue());
+		restaurants.sort((r1, r2) -> r2.getRate() - r1.getRate());	
 		restaurants.forEach(r -> {
 			System.out.println(r.getName() + "," + r.getRate() + "," + r.getJustification());
 		});
 
-		return restaurants;
-		/*result.getRestaurants().forEach(r -> {
-			System.out.println("Result" + r.getName() + "," + r.getRate());
-		});*/
+		return restaurants;		
 	}
 }
